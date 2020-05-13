cf49a10bfaec27b623db148f2f8d5fba7461c362
dandycheng <dandy.ckb24@gmail.com>
2020-05-14 03:16:44 +0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let converter = new showdown.Converter({simplifiedAutoLink:true})
+let converter = new showdown.Converter({ simplifiedAutoLink: true })
+
 converter.getOption({ simplifiedAutoLink: true })
 $('#editor').keyup(function () {
     $('#output').html(converter.makeHtml($(this).val()))
+})
+
+
+window.html2canvas = html2canvas
+let pdf = new jsPDF()
+let pdfOptions = {
+    filename:'test.pdf',
+    margin:10
+}
+
+
+$('#export').click(async function () {
+    html2pdf($('#output').html(),pdfOptions)
 })
