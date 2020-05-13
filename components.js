79f7d4c61e43a097fbfa66418acc97b370de8c2d
Dandy Cheng <dandy.ckb24@gmail.com>
2020-05-14 03:25:59 +0800

    Delete components.js

--- components.js
+++
@@ -1,8 +0,0 @@
-const markdownRegex = {
-    '^#+\s+[\S]+$': (content) => {
-        let nHash = content.match(/^#+/g).split('').length
-        let tag = nHash <= 4 ? `h${nHash}` : 'p'
-        `<${tag}>${ content }</${tag}>`,
-    },
-    ''
-}
